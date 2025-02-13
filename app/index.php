25d8c1655d0d558090e84aae2a550fa2c0a36529
Preksha <ranapreksha8@gmail.com>
2025-02-13 17:12:56 +0530

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,17 +1,7 @@
 <?php
 require('functions.php');
-require('router.php');
 require('Database.php');
+require('Response.php');
+require('router.php');
 
-$config = require('config.php');
-$db = new Database($config['database']);
-
-$title = $_GET['title'];
-//dd($id);
-$query = "select * from posts where title=?";
-$items = $db->query($query, [$title])->fetch();
-dd($items);
-// foreach($items as $item){
-//     echo '<li>'.$item['title'].'</li>';
-// }
 ?>
