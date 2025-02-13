7e5615864bbee3427025cd352ab7cd4a1cd0901a
Preksha <ranapreksha8@gmail.com>
2025-02-13 17:13:49 +0530

    Create 403.php

---
+++ app/views/403.php
@@ -0,0 +1,12 @@
+<?php require('views/partials/head.php') ?>
+<?php require('views/partials/nav.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">You are not authorized to view.</h1>
+
+        <p class="mt-4">
+            <a href="/" class="text-blue-500 underline">Go back home.</a>
+        </p>
+    </div>
+</main>
