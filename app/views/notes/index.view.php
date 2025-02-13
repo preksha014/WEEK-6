aada9c84cc1ea0be0b14769bb78ac4f48effb082
Preksha <ranapreksha8@gmail.com>
2025-02-13 17:13:54 +0530

    Create index.view.php

---
+++ app/views/notes/index.view.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php require'views/partials/head.php'; ?>
+<?php require'views/partials/nav.php'; ?>
+<?php require'views/partials/banner.php'; ?>
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <h1>Welcome to notes page!</h1>
+    <ul>
+    <?php foreach($notes as $note): ?>
+      <a href="/note?id=<?= $note['id']?>" class="text-blue-500 hover:underline"> 
+        <li><?=htmlspecialchars($note['body'])?></li>
+        </a>
+      <?php endforeach;?>
+
+    </ul>
+    <div class="mt-4">
+      <a href="/note/create" class="text-blue-500 hover:underline"><p>Create Note</p></a>
+    </div>
+    
+  </div>
+</main>
+</div>
+</body>
+
+</html>
