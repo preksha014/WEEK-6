35e5456fd6c12ef83ff80abdc25f286e8c075e2a
Preksha <ranapreksha8@gmail.com>
2025-02-13 17:12:42 +0530

    Update nav.php

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -14,7 +14,9 @@
               aria-current="page">Home</a>
             <a href="/about"
               class="<?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-            <a href="/contact"
+              <a href="/notes"
+              class="<?= urlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Notes</a>
+              <a href="/contact"
               class="<?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
           </div>
         </div>
