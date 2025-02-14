2d28d82f98311e6e938f4a34ccd3c9ddfe526588
Preksha <ranapreksha8@gmail.com>
2025-02-14 17:53:27 +0530

    Create bootstrap.php

---
+++ app/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+use Core\Database;
+use Core\Container;
+
+$container=new Container();
+
+$container->bind('Core\Database',function(){
+    $config = require base_path('config.php');
+    return new Database($config['database']);
+
+});
+
+// $db=$container->resolve('Core\Database');
